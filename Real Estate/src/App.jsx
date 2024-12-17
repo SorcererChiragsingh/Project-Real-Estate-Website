2234cbcd57805ca04b59a231ea19dc4a73d54167
Chirag singh <chiragsingh8926@gmail.com>
2024-12-17 20:40:43 +0530

    changes in app.jsx file

--- Real Estate/src/App.jsx
+++ Real Estate/src/App.jsx
@@ -1,7 +1,8 @@
-export default function App() {
+import React from 'react'
+
+export const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Fuck You world!
-    </h1>
+    <div>App</div>
   )
 }
+
