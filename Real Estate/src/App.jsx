eda900bc9bd86de328405dcecd70bf1d261b4be3
Chirag singh <chiragsingh8926@gmail.com>
2024-12-30 23:24:45 +0530

    Dark mode

--- Real Estate/src/App.jsx
+++ Real Estate/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 // import Hero from './sections/Hero'
-
+import { DarkModeProvider } from './components/DarkModeContext'
 
 
 export const App = () => {
   return (
     <>
-    
+    <DarkModeProvider>
+      
+    </DarkModeProvider>
     </>
   )
 }
