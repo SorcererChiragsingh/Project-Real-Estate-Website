6ab8c30fcb0273063b91287a8b6628ac9056f54d
Chirag singh <chiragsingh8926@gmail.com>
2025-01-02 21:23:58 +0530

    Importing Files

--- Real Estate/src/App.jsx
+++ Real Estate/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-// import Hero from './sections/Hero'
 import { DarkModeProvider } from './components/DarkModeContext'
+import Header from './components/Header'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import PopularAreas from './sections/PopularAreas'
+
 
 
 export const App = () => {
   return (
     <>
     <DarkModeProvider>
-      
+      <Header />
+      <Hero />
+      <About  />  
+      <PopularAreas />
+
     </DarkModeProvider>
     </>
   )
