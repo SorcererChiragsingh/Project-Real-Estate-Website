8e34f76416eb58d7c38ea5fc0864c491a3763aff
Chirag singh <chiragsingh8926@gmail.com>
2025-01-05 22:45:04 +0530

    Hero.jsx file changes

--- Real Estate/src/sections/Hero.jsx
+++ Real Estate/src/sections/Hero.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDarkMode } from '../components/DarkModeContext'
+import Heroimg from '../assets/images/hero1.webp' 
+import aos from 'aos'
+import 'aos/dist/aos.css'
+
 
 export const Hero = () => {
+
+  useEffect(()=> {
+    Aos.init({
+      offset; 200,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100
+    });
+  }, [])
+
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
-    <div>Hero</div>
+    <>
+<div>
+
+</div>
+    </>
   )
 }
 
