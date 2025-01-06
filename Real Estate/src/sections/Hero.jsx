cdebbf002ff8bac6fa24a95852c98e6382e5ec6d
Chirag singh <chiragsingh8926@gmail.com>
2025-01-07 01:31:55 +0530

    Fixing error in assets

--- Real Estate/src/sections/Hero.jsx
+++ Real Estate/src/sections/Hero.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
 import { useDarkMode } from '../components/DarkModeContext'
-import heroimg from '../assets/images/hero1.webp' 
+import heroimg from '../assets/images/hero1.webp'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 
 export const Hero = () => {
 
-  useEffect(()=> {
+  useEffect(() => {
     AOS.init({
       offset: 200,
       duration: 800,
@@ -20,11 +20,14 @@ export const Hero = () => {
 
   return (
     <>
-<div className={`${darkMode ? 'dark bg-black' : 'light bg-white'}`}>
-<section id = "hero" className="hero d-flex flex-column justify-content-center align-items-center">
+      <div className={`${darkMode ? 'dark bg-black' : 'light bg-white'}`}>
+        <section id='hero' className='w-[95%] h-[600px] m-auto bg-cover bg-center rounded-x1 flex justify-center flex-col items-start lg:px-28 px-10 gap-7 z-20'
+          style={{ backgroundImage: `url(${hero})` }}>
+<h1 data-aos="zoom-in" className="text-6x1 text-white font-semibold lg:pr-[500px] pr-0 lg:leading-[70px] leading-[60px]">Find Your Dream Home</h1>
 
-</section>
-</div>
+
+        </section>
+      </div>
     </>
   )
 }
